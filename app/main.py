04e9e432d47497119551f1d1f60d5566ac87d219
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-04-03 13:41:05 +0300

    herbivores-and-carnivores

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(
+            self, name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self):
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int | None:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -17,20 +21,24 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
-    def catch_mouse(self):
+    def catch_mouse(self) -> None:
         print("The hunt began!")
 
+
 class Dog(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
 
-    def bring_slippers(self):
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
-def feed_animals(animals):
+
+from typing import Iterable, Union
+
+def feed_animals(animals: Iterable) -> Union[int, float]:
     return sum(animal.feed() for animal in animals)
 
 # Example usage:
